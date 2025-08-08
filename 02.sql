92e1c9bf62c58cdbe3b56df1e2a8633452a90f8f
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-08 18:19:04 +0530

    Do SQL 3

--- 02.sql
+++ 02.sql
@@ -8,6 +8,19 @@ FROM
 WHERE
     IFNULL(referee_id, 0) <> 2
 
+/*  if referee_id is not null give me the same I'll compare it with <>(not equal to) 2 else give me 0
+and I'll compare 0 != 2, right?
+
+IFNULL(expression, value) in MySQL
+IFNULL(x, y) checks:
+If x is NOT NULL, return x.
+If x is NULL, return y.
+
+*/
+
+
+
+
 /* another way */
 SELECT 
     name
