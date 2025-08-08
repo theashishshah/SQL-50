478dfbcf350bae647d4db9ec45d1d03a724d6809
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-08 18:13:05 +0530

    Do SQL 2 (Optimized)

--- 02.sql
+++ 02.sql
@@ -7,3 +7,12 @@ FROM
     Customer
 WHERE
     IFNULL(referee_id, 0) <> 2
+
+/* another way */
+SELECT 
+    name
+FROM
+    Customer
+WHERE 
+    referee_id IS NULL OR referee_id <> 2
+    /* <> is the original sql "not equal to"
