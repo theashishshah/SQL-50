67b77a6ab989df1e81e0323f31787efaf76ef1fc
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-08 23:46:24 +0530

    Do SQL 6 (other way around)

--- 06.sql
+++ 06.sql
@@ -12,3 +12,14 @@ ON
 give me the whole left table's employees's name and whichever is matching from the right table, give me that id else it will be null.
 
 */
+
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+
+
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+LEFT JOIN Product p
+USING(product_id)
