89f9ca7ae8d376c5ffe8bdf5a7104ba0d389f708
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-08 18:22:12 +0530

    Do SQL 3 (with union)

--- 03.sql
+++ 03.sql
@@ -4,3 +4,20 @@ FROM
     World
 WHERE
     area >= 3000000 OR population >= 25000000
+
+/** or we can implement it using union */
+SELECT
+    name, population, area
+FROM 
+    World
+WHERE
+    area >= 3000000 
+    
+UNION
+
+SELECT
+    name, population, area
+FROM 
+    World
+WHERE
+    population >= 25000000
