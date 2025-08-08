f7742e62692a9a34db847264aaca0787542dd6c3
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-09 01:03:17 +0530

    Do SQL 9 (Other way around)

--- 09.sql
+++ 09.sql
@@ -4,3 +4,13 @@ JOIN Weather w2
 WHERE
     DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND
     w1.temperature > w2.temperature
+
+
+
+/* OTHER WAY AROUND */
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+WHERE
+    SUBDATE(w1.recordDate, 1) = w2.recordDate AND
+    w1.temperature > w2.temperature
