b0d140afa67079b5dba6f934cb2a8694f2a31f43
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-09 22:59:12 +0530

    Do SQL 13 (Another way around)

--- 13.sql
+++ 13.sql
@@ -6,3 +6,12 @@ INNER JOIN Employee e2
 ON e1.id = e2.managerId
 GROUP BY e1.id) AS managers
 WHERE managerId >= 5
+
+
+/* another way around */
+SELECT e1.name
+FROM Employee e1
+INNER JOIN Employee e2
+ON e1.id = e2.managerId
+GROUP BY e2.managerId
+HAVING COUNT(e2.managerId) >= 5
