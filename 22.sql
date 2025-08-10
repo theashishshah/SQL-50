8e537c3c7d408faba11ce74ae97baa97e8d0bd32
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-10 16:59:34 +0530

    Do SQL 22

---
+++ 22.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+
+SELECT 
+    ROUND(
+        COUNT(player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity)
+    , 2) AS fraction
+FROM 
+    Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) 
+IN
+(SELECT  # First find out first logins
+    player_id, MIN(event_date) as first_login
+FROM
+    Activity
+GROUP BY
+    player_id
+    )
