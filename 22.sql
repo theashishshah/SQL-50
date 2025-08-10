c816e361e363b7f5a5caefdf5bbdc615dfa8a120
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-10 17:01:49 +0530

    Do SQL 22 (other way around)

--- 22.sql
+++ 22.sql
@@ -16,3 +16,22 @@ FROM
 GROUP BY
     player_id
     )
+
+
+
+/* Other way around */
+SELECT 
+    ROUND(
+        COUNT(DISTINCT a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM 
+    Activity a
+JOIN (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) first_logins
+ON a.player_id = first_logins.player_id
+WHERE a.event_date = DATE_ADD(first_logins.first_login, INTERVAL 1 DAY);
+
