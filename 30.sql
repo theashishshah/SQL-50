36a885ca39edf982cada4e2299cbb6b2ba5324d6
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-10 18:10:45 +0530

    Do SQL 30

---
+++ 30.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT 
+    e1.employee_id AS employee_id, 
+    e1.name AS name , 
+    COUNT(e2.employee_id) AS reports_count, 
+    ROUND(AVG(e2.age)) AS average_age
+FROM
+    Employees e1
+INNER JOIN
+    Employees e2
+ON 
+    e1.employee_id = e2.reports_to
+GROUP BY
+    e1.employee_id
+ORDER BY
+    e1.employee_id ASC
