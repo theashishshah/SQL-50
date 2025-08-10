16f966cf62c450e0e17c64c89b0c76a2f0abecb4
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-10 10:37:48 +0530

    Do SQL 18

---
+++ 18.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    r.contest_id, ROUND((COUNT(r.user_id) / (SELECT COUNT(*) FROM Users)) * 100, 2 ) AS percentage
+FROM
+    Users u
+RIGHT JOIN
+    Register r
+USING
+    (user_id)
+GROUP BY
+    r.contest_id
+ORDER BY 
+    percentage DESC, r.contest_id ASC
