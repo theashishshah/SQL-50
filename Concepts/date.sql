0e6301309566193d91631f6d1d1fa8d2dcf9a04e
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-10 11:27:30 +0530

    Learn date format

---
+++ Concepts/date.sql
@@ -0,0 +1,6 @@
+| Expression                      | Output       | Description              |
+| ------------------------------- | ------------ | ------------------------ |
+| `DATE_FORMAT(date, "%Y-%m-%d")` | `2025-08-09` | Full date                |
+| `DATE_FORMAT(date, "%b %Y")`    | `Aug 2025`   | Abbreviated month + year |
+| `DATE_FORMAT(date, "%W")`       | `Saturday`   | Day of week              |
+| `DATE_FORMAT(date, "%H:%i")`    | `15:30`      | Time (24h)               |
