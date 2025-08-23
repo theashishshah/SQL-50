ffe957a4c9cdf610c860e7c33b3c91bae18c9777
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-23 23:35:09 +0530

    Do SQL 36

---
+++ 36.sql
@@ -0,0 +1,13 @@
+SELECT "High Salary" AS category, COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000
+
+UNION ALL
+SELECT "Low Salary" , COUNT(*)
+FROM Accounts
+WHERE income < 20000
+
+UNION ALL
+SELECT "Average Salary" , COUNT(*)
+FROM Accounts
+WHERE income BETWEEN 20000 AND 50000
